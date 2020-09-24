97702ad03bb05d6e9fb3e206adfdb0128e1c7769
HypeByte <69880284+HypeByte@users.noreply.github.com>
2020-09-23 18:13:57 -0700

    Game Intro Animation

--- Screens/Intro/intro.js
+++ Screens/Intro/intro.js
@@ -1,9 +1,10 @@
 var AM = document.getElementById("AM");
 var offset = 4525;
+
 function Animate() {
-    offset = offset-20;
+    offset = offset - 20;
     window.requestAnimationFrame(Animate);
-    AM.style.strokeDasharray = 4525,4525
+    AM.style.strokeDasharray = 4525, 4525
     AM.style.strokeDashoffset = offset;
 }
 
