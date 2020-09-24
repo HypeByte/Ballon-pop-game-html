4f913c14917e01c6576494909b99951fb04c037a
HypeByte <69880284+HypeByte@users.noreply.github.com>
2020-09-23 18:16:23 -0700

    Color switch animation

--- Screens/Intro/Game/Home/home.js
+++ Screens/Intro/Game/Home/home.js
@@ -1,15 +1,15 @@
 var GH = document.getElementById("GH");
-var GH_COLORS = ["#613DC1","#FB4D3D","#00A7E1","#6EFF51","#FFF47C"];
+var GH_COLORS = ["#613DC1", "#FB4D3D", "#00A7E1", "#6EFF51", "#FFF47C"];
 
-var i =-1;
+var i = -1;
 
 function ColorSwitch() {
-i = i+1;
-if(i === 5){
-    i = 0;
-}
-GH.style.color = GH_COLORS[i];
+    i = i + 1;
+    if (i === 5) {
+        i = 0;
+    }
+    GH.style.color = GH_COLORS[i];
 };
 
 ColorSwitch();
-var int = setInterval(ColorSwitch,1000);
+var int = setInterval(ColorSwitch, 1000);
